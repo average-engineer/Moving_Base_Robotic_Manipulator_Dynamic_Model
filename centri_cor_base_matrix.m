612ca3adbad165a1cb1246db312182f53aa3b994
Ashutosh Mukherjee <ashutosh.mukherjeecpg@gmail.com>
2022-04-17 13:19:07 +0200

    Modified the seemingly wrong expression for the PV matrix (matrix for accounting the centrifugal and coriolis force effects of the moving base)

--- centri_cor_base_matrix.m
+++ centri_cor_base_matrix.m
@@ -1,14 +1,24 @@
 function [p_v] = centri_cor_base_matrix(sim_time,U,J,T_b,T_b_dot,NJ)
 
 for ii = 1:length(sim_time)
+%     for i = 1:NJ
+%         for r = 1:NJ
+%             p_v{ii}(i,r) = 0;
+%             for j = max(i,r):NJ
+%                 pv_1 = 2*trace(T_b_dot{ii}*U{ii}{j,r}*J{ii}{j}*(U{ii}{j,i}')*(T_b{ii}'));
+%                 p_v{ii}(i,r) = pv_1 + p_v{ii}(i,r);
+%             end
+%         end 
+%     end
+
     for i = 1:NJ
-        for r = 1:NJ
-            p_v{ii}(i,r) = 0;
-            for j = max(i,r):NJ
-                pv_1 = 2*trace(T_b_dot{ii}*U{ii}{j,r}*J{ii}{j}*(U{ii}{j,i}')*(T_b{ii}'));
-                p_v{ii}(i,r) = pv_1 + p_v{ii}(i);
+        for j = i:NJ
+            for r = 1:j
+                p_v{ii}(i,r) = 0;
+                pv_1 = trace(T_b_dot{ii}*U{ii}{j,r}*J{ii}{j}*(U{ii}{j,i}')*(T_b{ii}'));
+                p_v{ii}(i,r) = pv_1 + p_v{ii}(i,r);
             end
-        end 
+        end
     end
 end
 end
